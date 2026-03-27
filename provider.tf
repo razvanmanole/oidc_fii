0f147bdaa6c892f067af6d7849e403b8a7ae9c2d
IustinAchitenei <113167994+IustinAchitenei@users.noreply.github.com>
2026-03-27 11:50:23 +0000

    clean

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "fiipracti-state654687354"
+    bucket  = "YOUR_BUCKET_NAME"
     key     = "terraform.tfstate"
-    region  = "eu-central-1"
+    region  = "eu-north-1"
     encrypt = true
   }
 }
